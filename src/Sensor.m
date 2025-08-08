8fac0a19c10397cf8ed01c9c08bf9cbbcfab088d
nelson10 <38512291+nelson10@users.noreply.github.com>
2025-08-08 12:36:34 -0400

    Add files via upload

--- src/Sensor.m
+++ src/Sensor.m
@@ -1,4 +1,4 @@
-function [Y,I1,T2,I2] = Sensor(I,T,sigma,shifting)
+function [Y,T2,I2] = Sensor(I,T,sigma,detector)
 N1 = size(T,2);
 N3 = size(T,1);
 K = size(I,3);
@@ -8,35 +8,50 @@ rng("default")
 
 for j=1:D
     for i=1:K
-        I1(:,:,i,j) = imresize(I(:,:,i,j),[N1 N1],"box");
+        I1(:,:,i,j) = imresize(I(:,:,i,j),[N1 N1],"box");  % original
     end
 end
 
-if(shifting==1)
-    temp = rand(N3,N3)>=0.5;
+if(detector==0)
+    Y = zeros(N1,N1,D);
+    I2 = I1;
+    T2 = T;
+elseif(detector==1)
+    temp = rand(N3,N3)>=0.5; % Coded aperture with 50% transmittance
     for j=1:D
         for i=1:K
             I2(:,(i-1)+1:(i-1)+N1,i,j) = I1(:,:,i,j);
-            T2(:,(i-1)+1:(i-1)+N1,i) = temp;%T(:,:,1);
+            T2(:,(i-1)+1:(i-1)+N1,i) = temp;
         end
     end
     [N2,N3,~] =size(T2);
     Y = zeros(N2,N3,D);
-else
-    Y = zeros(N1,N1,D);
-    I2 = I1;
+elseif(detector==2)
+    for j=1:D
+        for i=1:K
+            noise = normrnd(0,sigma,[N1,N1]);
+            y1 = I1(:,:,i,j) + noise;
+            y1(y1<=0)=0;
+            y1(y1>=1) = 1;
+            I1(:,:,i,j) = y1.*dynamicRange;
+        end
+    end
+    Y = I1;
     T2 = T;
+    I2 = I1;
 end
 
-for j=1:D
-    Y1 = I2(:,:,:,j).*T2;
-    Y(:,:,j) = mat2gray(sum(Y1,3));
-    [m1,n1,~] = size(Y);
-    noise = normrnd(0,sigma,[m1,n1]);
-    y1 = Y(:,:,j) +  noise;
-    y1(y1<=0)=0;
-    y1(y1>=1) = 1;
-    Y(:,:,j) = y1.*dynamicRange;
+if(detector==0 || detector==1)
+    for j=1:D
+        Y1 = I2(:,:,:,j).*T2;
+        Y(:,:,j) = mat2gray(sum(Y1,3));
+        [m1,n1,~] = size(Y);
+        noise = normrnd(0,sigma,[m1,n1]);
+        y1 = Y(:,:,j) +  noise;
+        y1(y1<=0)=0;
+        y1(y1>=1) = 1;
+        Y(:,:,j) = y1.*dynamicRange;
+    end
 end
 
 % noise = normrnd(0,sigma,[N1,N1]);
