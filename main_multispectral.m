22a6b82c1ce4af4ce2e0cad31b23ba60d2af5ed5
nelson10 <38512291+nelson10@users.noreply.github.com>
2025-08-08 12:34:32 -0400

    Add files via upload

--- main_multispectral.m
+++ main_multispectral.m
@@ -19,7 +19,7 @@ addpath(genpath('./metrics'));
 %addpath(genpath('./Kodak24/'));
 addpath(genpath('./McMaster/'));
 addpath(genpath('./TwIST_v2/'));
-addpath(genpath('C:/Users/nelson/OneDrive/Documenten/DeSCI-master-multispectral/dataset'));
+addpath(genpath('./dataset/'));
 
 a = 1e0; % 1e0 [m]  1e3 [mm]
 r = 2.5e-3.*a; % radius of the pupil % 2.5e-3 or 3.0e-3
@@ -28,8 +28,12 @@ N = round(2*r/doe_pitch);  % Number of grid points per side   %2464
 sigma_d = 3e-8;
 sigma_s = [0.005, 0.009, 0.015, 0.020];
 sigma = sigma_s(2);
-shifting = 1; % 0 MSFA, 1 CASSI
-algo = 2; %deblurring algorithm, 1 Richarson Lucy, 2 L2-TV
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+detector = 1; % 0 MSFA, 1 CASSI, 2 Pushbroom
+algo = 1; %deblurring algorithm, 1 Richarson Lucy, 2 L2-TV
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 ploton = 1; % To depict groundtruth, mesurement, psf, Doe, Wiener filter recovery
 showMTF = 1; % 1 To show MTF otherwise PSF#
 dynamicRange = 2^8-1;
@@ -39,7 +43,7 @@ mult = 1;
 N1 = 512;
 L = 31;
 [T]=regularMultiplexedSpherePackingCodedAperture(N1,L,mult);
-idx = [0 5 6 8 1];
+idx = [7 1 0 5 6 8 ];
 for i=1:5
     diffractive = idx(i); %0 Akpinar TIP-2021, 1 Ours, 2 Spiral-Jeon, 3 Fresnel, 4 without DOE, 5 Dowski-Cathey,6 Oliva,7 Helical axicon, 8 Pinilla et al
     DOE = loadDoe(diffractive,N,sigma_d,doe_pitch);
@@ -48,6 +52,7 @@ for i=1:5
     %max(DOE(:))
     length(unique(DOE(:)))
     [PSF,deltaS] = computePSF2(DOE);
+    Ns = size(PSF,2);
 
     p = zeros(32,4);
     s = zeros(32,4);
@@ -58,17 +63,12 @@ for i=1:5
         load(dataset);
         I = mat2gray(double(hyperimg))*dynamicRange;
         [Y_md] = DOE_effect(I,PSF,deltaS);
-        [Y,I_orig,T1,I2] = Sensor(Y_md,T,sigma,shifting);
-        
-        if(shifting==1)
-            [Xrec] = reconstruction(Y,T1,I2,shifting);
-        else
-            [Xrec] = recover(Y,T);
-            %[Xrec] = reconstruction(Y,T1,I_orig,shifting); % GAP-TV to recover the multispectral datacube
-        end
+        [Y,T1,I2] = Sensor(Y_md,T,sigma,detector);  %0 MSFA, 1 CASSI, 2 Pushbroom
+        [Xrec] = reconstruction(Y,T1,I2,detector);
         X = debluring(Xrec,PSF,algo); %Y_orig
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Metrics %%%%%%%%%%%%%%%%%%%%%%%%%%
         disp("Dataset="+num2str(l))
+        I = imresize(I,[Ns Ns],"box");
         for k=1:4
             [p(l,k),s(l,k),sam(l,k)] = metrics2(uint8(I),uint8(X(:,:,:,k)));
         end
