be3728ef4d882185baa51b51652ba7a12f5801af
nelson10 <38512291+nelson10@users.noreply.github.com>
2025-08-22 12:27:07 -0400

    Add files via upload

--- main_multispectral.m
+++ main_multispectral.m
@@ -16,7 +16,7 @@ addpath(genpath('./GAP-TV'));
 addpath(genpath('./utils2'));
 addpath(genpath('./RGB'));
 addpath(genpath('./metrics'));
-%addpath(genpath('./Kodak24/'));
+addpath(genpath('./Kodak24/'));
 addpath(genpath('./McMaster/'));
 addpath(genpath('./TwIST_v2/'));
 addpath(genpath('./dataset/'));
@@ -26,60 +26,74 @@ r = 2.5e-3.*a; % radius of the pupil % 2.5e-3 or 3.0e-3
 doe_pitch = 2.0292e-6;%1.86e-6; % DOE pitch
 N = round(2*r/doe_pitch);  % Number of grid points per side   %2464
 sigma_d = 3e-8;
-sigma_s = [0.005, 0.009, 0.015, 0.020];
-sigma = sigma_s(2);
+sigma_s = [0 0.005, 0.009, 0.015, 0.020];
+sigma = sigma_s(1);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-detector = 1; % 0 MSFA, 1 CASSI, 2 Pushbroom
+detector = 2; % 0 MSFA, 1 CASSI, 2 Pushbroom, 3 RGB
 algo = 1; %deblurring algorithm, 1 Richarson Lucy, 2 L2-TV
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-ploton = 1; % To depict groundtruth, mesurement, psf, Doe, Wiener filter recovery
+ploton = 0; % To depict groundtruth, mesurement, psf, Doe, Wiener filter recovery
 showMTF = 1; % 1 To show MTF otherwise PSF#
 dynamicRange = 2^8-1;
-alldataset = {'balloons_ms','beads_ms','cd_ms','chart','clay_ms','cloth_ms','egyptian_statue_ms','feathers_ms','flowers_ms','glass_tiles_ms','pompoms_ms','sponges_ms','stuffed_toys_ms','superballs_ms','thread_spools_ms','fake_and_real_beers_ms','face_ms','real_and_fake_peppers_ms','real_and_fake_apples_ms','photo_and_face_ms','paints_ms','oil_painting_ms','jelly_beans_ms','hairs_ms','fake_and_real_tomatoes_ms','fake_and_real_sushi_ms','fake_and_real_strawberries_ms','fake_and_real_peppers_ms','fake_and_real_lemons_ms','fake_and_real_lemon_slices_ms','fake_and_real_food_ms','watercolors_ms'};
 
-mult = 1;
 N1 = 512;
 L = 31;
-[T]=regularMultiplexedSpherePackingCodedAperture(N1,L,mult);
-idx = [7 1 0 5 6 8 ];
-for i=1:5
-    diffractive = idx(i); %0 Akpinar TIP-2021, 1 Ours, 2 Spiral-Jeon, 3 Fresnel, 4 without DOE, 5 Dowski-Cathey,6 Oliva,7 Helical axicon, 8 Pinilla et al
-    DOE = loadDoe(diffractive,N,sigma_d,doe_pitch);
+idx = [1 0 5 8 6 4];%[7 1 0 5 6 8];
+
+for i=1:6
+    diffractive = idx(i); %0 Akpinar TIP-2021, 1 Diaz, 2 Spiral-Jeon, 3 Fresnel, 4 without DOE, 5 Dowski-Cathey,6 Oliva,7 Helical axicon, 8 Pinilla et al
+    [DOE,doeName] = loadDoe(diffractive,N,sigma_d,doe_pitch);
     %imagesc(DOE),pbaspect([1 1 1]),colormap('jet')
     %imagesc(DOE(426:end-426,426:end-426)),pbaspect([1 1 1])
     %max(DOE(:))
     length(unique(DOE(:)))
-    [PSF,deltaS] = computePSF2(DOE);
+    [PSF,deltaS] = computePSF2(DOE,L);
     Ns = size(PSF,2);
 
-    p = zeros(32,4);
-    s = zeros(32,4);
-    sam = zeros(32,4);
+    K = size(PSF,4);
+    p = zeros(32,K);
+    s = zeros(32,K);
+    sam = zeros(32,K);
 
     for l=1:32
-        dataset = alldataset{l};
-        load(dataset);
-        I = mat2gray(double(hyperimg))*dynamicRange;
+        [I] = loadImage(detector,l,dynamicRange);
         [Y_md] = DOE_effect(I,PSF,deltaS);
-        [Y,T1,I2] = Sensor(Y_md,T,sigma,detector);  %0 MSFA, 1 CASSI, 2 Pushbroom
+        [Y,T1,I2,detectorName] = Sensor(Y_md,N1,sigma,detector);  %0 MSFA, 1 CASSI, 2 Pushbroom, 3 RGB
         [Xrec] = reconstruction(Y,T1,I2,detector);
         X = debluring(Xrec,PSF,algo); %Y_orig
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Metrics %%%%%%%%%%%%%%%%%%%%%%%%%%
         disp("Dataset="+num2str(l))
         I = imresize(I,[Ns Ns],"box");
-        for k=1:4
+        K = size(p,2);
+        for k=1:K
             [p(l,k),s(l,k),sam(l,k)] = metrics2(uint8(I),uint8(X(:,:,:,k)));
         end
-        showPlot2(I,X,Y_md,DOE,PSF,l,showMTF,ploton,algo);
+        %showPlot2(I,X,Y_md,DOE,PSF,l,showMTF,ploton,algo);
     end
 
     pm = mean(mean(p,2));
     sm = mean(mean(s,2));
     samm = mean(mean(sam,2));
 
-    save("DOE="+num2str(diffractive)+"_Algo="+num2str(algo)+"noise-measure="+num2str(sigma)+'.mat','p','s','sam')
+    save("DOE=_"+num2str(diffractive)+doeName+"_AlgoDebluring=_"+num2str(algo)+"_Detector=_"+detectorName+"_noise-measure=_"+num2str(sigma)+'.mat','p','s','sam')
+    dist = ["z1", "z2", "z3", "z4", "z5", "z6", "avg"];
+    PSNR_mean = mean(p, 1);
+    PSNR_std  =  std(p, 1);
+    SSIM_mean = mean(s, 1);
+    SSIM_std  =  std(s, 1);
+    SAM_mean = mean(sam, 1);
+    SAM_std  =  std(sam, 1);
+    avg_PSNR = mean(p, 'all');
+    avg_SSIM = mean(s, 'all');
+    avg_SAM  = mean(sam, 'all');
+    std_PSNR = mean(PSNR_std);
+    std_SSIM = mean(SSIM_std);
+    std_SAM  = mean(SAM_std);
+    varNames = {'dist', 'PSNR_avg', 'PSNR_std', 'SSIM_avg', 'SSIM_std', 'SAM_avg', 'SAM_std'};
+    Tabla = table(dist', [PSNR_mean avg_PSNR]',[PSNR_std std_PSNR]' , [SSIM_mean avg_SSIM]', [SSIM_std std_SSIM]', [SAM_mean avg_SAM]', [SAM_std std_SAM]', 'VariableNames',varNames )
+    writetable(Tabla, names_sensor(shifting+1) +"_"+names_DOE(DOE_idx)+"_"+Algorithm(algo)+"_sigmaID_"+num2str(sigma_id)+"_full.csv")
 end
 
 % X = double(X);
