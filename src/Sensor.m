1d8e58989121f461439870e619c46a078956bc40
nelson10 <38512291+nelson10@users.noreply.github.com>
2025-08-22 12:29:50 -0400

    Add files via upload

--- src/Sensor.m
+++ src/Sensor.m
@@ -1,6 +1,15 @@
-function [Y,T2,I2] = Sensor(I,T,sigma,detector)
-N1 = size(T,2);
-N3 = size(T,1);
+function [Y,T2,I2,detectorName] = Sensor(I,N,sigma,detector)
+%% Outputs
+% Y -> Measurement
+% T2 -> Coded Aperture
+% I2 -> Groundtruth
+
+%% Inputs
+% I -> Original Imagge
+% N -> Spatial resolution
+% sigma -> Standard desviation of the noise
+% detector -> type of detector
+
 K = size(I,3);
 D = size(I,4);
 dynamicRange = 2^8-1;
@@ -8,28 +17,34 @@ rng("default")
 
 for j=1:D
     for i=1:K
-        I1(:,:,i,j) = imresize(I(:,:,i,j),[N1 N1],"box");  % original
+        I1(:,:,i,j) = imresize(I(:,:,i,j),[N N],"box");  % original
     end
 end
 
-if(detector==0)
-    Y = zeros(N1,N1,D);
+if(detector==0) % MSFA
+    Y = zeros(N,N,D);
     I2 = I1;
+    mult = 1;
+    %[T]=regularMultiplexedSpherePackingCodedAperture(N,K,mult);
+    %save("MSFA.mat","T");
+    load("MSFA.mat","T");
     T2 = T;
-elseif(detector==1)
-    temp = rand(N3,N3)>=0.5; % Coded aperture with 50% transmittance
+    detectorName = "MSFA";
+elseif(detector==1) % CASSI
+    temp = rand(N,N)>=0.5; % Coded aperture with 50% transmittance
     for j=1:D
         for i=1:K
-            I2(:,(i-1)+1:(i-1)+N1,i,j) = I1(:,:,i,j);
-            T2(:,(i-1)+1:(i-1)+N1,i) = temp;
+            I2(:,(i-1)+1:(i-1)+N,i,j) = I1(:,:,i,j);
+            T2(:,(i-1)+1:(i-1)+N,i) = temp;
         end
     end
     [N2,N3,~] =size(T2);
     Y = zeros(N2,N3,D);
-elseif(detector==2)
+    detectorName = "CASSI";
+elseif(detector==2) %Pushbroom
     for j=1:D
         for i=1:K
-            noise = normrnd(0,sigma,[N1,N1]);
+            noise = normrnd(0,sigma,[N,N]);
             y1 = I1(:,:,i,j) + noise;
             y1(y1<=0)=0;
             y1(y1>=1) = 1;
@@ -37,11 +52,22 @@ elseif(detector==2)
         end
     end
     Y = I1;
-    T2 = T;
+    T2 = ones(N,N,K);
     I2 = I1;
+    detectorName = "Pushbroom";
+elseif(detector==3) %RGB
+    K = [3 2; 2 1];
+    M = kron(K,ones(N/2,N/2));
+    for i=1:3
+        T(:,:,i) = (M == i);
+    end
+    %'rggb', 'bggr', 'gbrg', 'grbg'
+    Y = zeros(N,N,D);
+    I2 = I1;
+    T2 = T;
 end
 
-if(detector==0 || detector==1)
+if(detector==0 || detector==1 || detector==3) % MSFA, CASSI
     for j=1:D
         Y1 = I2(:,:,:,j).*T2;
         Y(:,:,j) = mat2gray(sum(Y1,3));
--- src/loadDoe.m
+++ src/loadDoe.m
@@ -1,4 +1,4 @@
-function d = loadDoe(diffractive,N,sigma_d,doe_pitch)
+function [d,doeName] = loadDoe(diffractive,N,sigma_d,doe_pitch)
 rng("default")
 height_max = 1.3745e-6; %2.4e-6;
 if(diffractive == 0)
@@ -13,11 +13,14 @@ if(diffractive == 0)
     d = mod(d*N1 , N1);
     d = floor(d);
     d = mat2gray(d) .* height_max; %2.4e-6;
+    doeName = "Akpinar";
 elseif(diffractive == 1)
     % load('MTF_cubic_zeni_-0.3_10.mat'); % 25.5379
     %load('MTF_cubic_zeni_0.1_10_94'); % 25.9567
     %load('MTF_cubic_zeni_0.1_10_best') % 23.8863
-    load('MTF_cubic_zeni_0.1_10_previous') % 25.9792
+    %load("/Users/nelsondiaz/Downloads/MSFA-DOE-main/DOEs/MTF_cubic_zeni_0.1_10_previous.mat") % 25.9792
+    %load("/Users/nelsondiaz/Downloads/MSFA-DOE-main/DOEs/MTF_cubic_zeni_0.1_10_sum_std_per_depth_115_zernikes.mat")
+    load("MTF_cubic_zeni_0.1_10_sum_std_per_depth_115_zernikes_double-vol.mat")
     %load('MTF_cubic_zeni_-0.3_10.mat') % 25.5379
     %load('MTF_cubic_zeni_0.4_40') % 25.4165
     %load('MTF_helical_zeni_0.1_10'); % 27.7628
@@ -30,13 +33,15 @@ elseif(diffractive == 1)
     d = mod(d*N1 , N1);
     d = floor(d);
     d = mat2gray(d) .* height_max;
+    doeName = "Diaz";
 elseif(diffractive == 2)
     %Paper: Compact Snapshot Hyperspectral Imaging with Diffracted Rotation
     %Authors: Jeon, Baek, Yi, Dun, Fu, Heidrich, and Kim
     load('spiral_DOE3180_16.mat');
     d = h;
     d = imresize(d,[N N],"nearest");
-    d = mat2gray(d).*0.9e-06;
+    d = mat2gray(d).*height_max;%0.9e-06;
+    doeName = "Spiral-Jeon";
 elseif(diffractive == 3)
     alpha = 10*pi; % 5 10 -> inf % 10 d= 1-d -> 0.5 0.8
     M = 1800;
@@ -58,6 +63,7 @@ elseif(diffractive == 3)
     %load('Uniform-DOE_1800-fres-0.5-0.8.mat')
 elseif(diffractive == 4)
     d = zeros(N,N);
+    doeName =  "without-DOE";
 elseif(diffractive == 5)
     %Paper: Extended depth of field through wave-front coding
     % %AuthorS: Edward R. Dowski, Jr., and W. Thomas Cathey
@@ -76,6 +82,7 @@ elseif(diffractive == 5)
     d = mod(d*N1 , N1);
     d = floor(d);
     d = mat2gray(d) .* height_max;
+    doeName = "Dowski-Cathey";
     %save('./DOEs/Dowski-Cathey_test.mat','d')
     %load('Dowski-Cathey_test.mat')
     %imagesc(d1-mat2gray(d)*height_max)
@@ -90,6 +97,7 @@ elseif(diffractive==6)
     d0 = imresize(d0, [N, N], 'nearest');
     d = (d0 - min(d0(:))) / (max(d0(:)) - min(d0(:)));
     d = d * height_max;
+    doeName = "Oliva";
 elseif(diffractive==8)
     load('Phase_delay_profile_6mm.mat');  % no full
     G3 = Phase_delay_profile_6mm_crop_middle;
@@ -109,6 +117,7 @@ elseif(diffractive==8)
     d = mod(d*N1 , N1);
     d = floor(d);
     d = mat2gray(d) .* height_max;
+    doeName = "Pinilla";
 elseif(diffractive==7)
     %CGH for generation and focusing of a helical wavefront
     L=1; % number of spirals
